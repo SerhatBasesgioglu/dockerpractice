6a9dc6b82296df74022c23f4727129f85238b22c
Serhat Başesgioğlu <55340640+SerhatBasesgioglu@users.noreply.github.com>
2024-08-08 12:29:28 +0300

    initial spring setup

---
+++ src/test/java/com/serhat/dockerpractice/DockerpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serhat.dockerpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
